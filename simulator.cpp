223f5deaca1c455f949eb79e367580e43d4f65c4
wadeepat <leaw.wadeepat@gmail.com>
2021-09-21 18:22:52 +0700

    finished lw,beq,add,halt,noop

--- simulator.cpp
+++ simulator.cpp
@@ -17,8 +17,10 @@ typedef struct stateStruct {
 
 void printState(stateType *);
 int convertNum(int);
-char* dectoBi(int);
+char* dectoBi(int,char *);
 int bitoDec16bit(char*);
+void iType(stateType *,char*);
+void rType(stateType *,char*);
 
 
 int main(int argc, char *argv[])
@@ -57,37 +59,50 @@ int main(int argc, char *argv[])
 
    
 
-     char *bi;
-     bi[0]='1';
-     cout<<"start bi";
-     int num = bitoDec16bit(bi);
-     cout<<num;
+     char bi[32]="1000000000000000";
+     
     // for(int i=31;i>=0;i--){
     //     cout<<bi[i];
     // }
     //cout<<bi[31]<<endl;
+    
+    int count = 0; char* temp;
     while(running){
-        char *instruc = dectoBi(state.mem[state.pc]);
-        cout<<instruc<<endl;
+        count++;
+        printState(&state);
+        //cout<<state.pc<<endl;
+        //cout<<"start state = " << <<endl;
+        char *instruc = dectoBi(state.mem[state.pc],temp);
+        //cout<<instruc<<endl;
+        //cout<<"test";
         //printf("instruc= %c",instruc);
-        printf("test %c %c \n",instruc[24],instruc[23]);
+        //printf("test %c %c \n",instruc[24],instruc[23]);
         if(instruc[24]=='0' && instruc[23]=='0'){
             //r
-           // rType(&state,instruc);
-           cout<<"in r";
+            rType(&state,instruc);
+            //cout<<"in r"<<endl;
         }else if(instruc[24]=='1'&& instruc[23]=='0' && instruc[22]=='1'){
             //j
         }else if(instruc[24]=='1'&& instruc[23]=='1'){
             //o
+            if(instruc[22]=='0'){
+                //halt
+                state.pc++;
+                cout<<"machine halted\ntotal of "<<count<<" instructions executed\nfinal state of machine:\n";
+                running=false;
+            }
         }else{
             //i
-            cout<<"in i";
+            //cout<<"in i"<<endl;
+            iType(&state,instruc);
         }
-        break;
+        //printState(&state);
+        
+        //cout<<"count= "<<count<<endl;
+        //if(count==6)running = false;
     }
     
     printState(&state);
-
     return(0);
 }
 
@@ -119,11 +134,12 @@ int convertNum(int num){
     return(num);
 }
 
-char* dectoBi(int dec){
-    //cout<<dec<<endl;
-    char *temp; 
+char* dectoBi(int dec,char *temp){
+    //cout<<dec<<endl; 
     int i=0;
+    
     while(i!=32){
+        //cout<<temp[i]<<endl;
         if(dec!=0) {
             if(dec%2==0)temp[i]='0';
             else temp[i]='1'; 
@@ -132,36 +148,36 @@ char* dectoBi(int dec){
         //cout<<temp[i]<<endl;
         i++;  
     }  
-    cout<<""; //for what
+    //cout<<"finished"<<endl; //for what
     return temp;
 }
 
 int bitoDec3bit(char a,char b,char c){
     int ans = 0;
-    if(a = '1')ans+=4;
-    if(b = '1')ans+=2;
-    if(c = '1')ans+=1;
+    if(a == '1')ans+=4;
+    if(b == '1')ans+=2;
+    if(c == '1')ans+=1;
     return ans;
 }
 
 int bitoDec16bit(char* bi){
     int ans =0;
     for(int i=0;i<16;i++){
-        if(bi[i]='1')ans+= pow(2,i);
+        if(bi[i]=='1')ans+= pow(2,i);
     }
-    cout<<"ok";
+    //cout<<"ok";
     return ans;
 }
 
 void rType(stateType *statePtr,char* instruc){
     
-    //int rd = bitoDec3bit(instruc[2],instruc[1],instruc[0]);
+    int rd = bitoDec3bit(instruc[2],instruc[1],instruc[0]);
 
     if(instruc[22]=='0'){//add
-        // int regA = bitoDec3bit(instruc[21],instruc[20],instruc[19]);
-        // int regB = bitoDec3bit(instruc[18],instruc[17],instruc[16]);
-        // statePtr->reg[rd] = statePtr->reg[regA]+statePtr->reg[regB];
-        // statePtr->pc++;
+        int regA = bitoDec3bit(instruc[21],instruc[20],instruc[19]);
+        int regB = bitoDec3bit(instruc[18],instruc[17],instruc[16]);
+        statePtr->reg[rd] = statePtr->reg[regA]+statePtr->reg[regB];
+        statePtr->pc++;
     }else{//nand
         char a,b,c;
         if(instruc[21]&&instruc[18])a='0';
@@ -174,7 +190,27 @@ void rType(stateType *statePtr,char* instruc){
 }
 
 void iType(stateType *statePtr,char* instruc){
+    int ra = bitoDec3bit(instruc[21],instruc[20],instruc[19]);
+    int rb = bitoDec3bit(instruc[18],instruc[17],instruc[16]);
+    int imm = convertNum(bitoDec16bit(instruc)); 
+    //cout<<imm<<endl;
+    //cout<<statePtr->mem[7]<<endl;
+    if(instruc[24]== '0'){
+        if(instruc[22]=='0'){
+            //lw
+            statePtr->reg[rb] = statePtr->mem[statePtr->reg[ra] + imm]; 
+            //cout<<ra<<" "<<rb<<" "<<statePtr->reg[ra+rb]<<endl;
+            statePtr->pc++;
+        }else{
+            //sw
 
+        }
+    }else{
+        //beq
+        //cout<<"beq "<<ra<<" "<<rb<<endl;
+        if(statePtr->reg[ra]==statePtr->reg[rb])statePtr->pc += 1+imm;
+        else statePtr->pc++;
+    }
     
 }
 void jType(stateType *statePtr,char* instruc){
