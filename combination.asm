67b956caaafed717ee643e121ed07fa9f3909ca3
MayJh4307 <norit.hunhan@hotmail.com>
2021-09-30 16:05:34 +0700

    edit by may round 1

--- combination.asm
+++ combination.asm
@@ -4,12 +4,12 @@
     lw 0 6 nCrAdr
     jalr 6 4         
     halt       
-nCr beq 2 0 ans1Ad
-    beq 2 1 ans1Ad
-    beq 2 5 ans2Ad
+nCr beq 2 0 ans1
+    beq 2 1 ans1
+    beq 2 5 ans2
     lw 0 6 neg1
     add 1 6 1     
-    beq 2 1 ans3Ad
+    beq 2 1 ans3
     sw 7 4 stack   
     add 7 5 7      
     sw 7 1 stack   
@@ -41,11 +41,11 @@ nCr beq 2 0 ans1Ad
     add 7 6 7
     lw 7 4 stack
     jalr 4 6
-ans1    add 3 pos1 3     
+ans1    add 3 5 3     
         jalr 4 6         
 ans2    add 3 1 3          
         jalr 4 6
-ans3    add 1 pos1 1     
+ans3    add 1 5 1     
         add 3 1 3
         jalr 4 6
 pos1 .fill 1
