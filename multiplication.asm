b362c99a9122d8be421ed9c25ce9345fbb0f50ea
WizAom <40514176+WizAom@users.noreply.github.com>
2021-10-04 18:28:02 +0700

    new

--- multiplication.asm
+++ multiplication.asm
@@ -1,20 +1,19 @@
     	lw 0 1 zero				
-		lw 0 2 mcand				
-		lw 0 3 mplier			
-		lw 0 4 one                           
+	lw 0 2 mcand				
+	lw 0 3 mplier			
+	lw 0 4 one                           
 multi	beq 7 3 finish			
-		nand 3 4 6	        
-		nand 6 6 6	
-		add 6 7 7   		      		
+	nand 3 4 6	        
+	nand 6 6 6	
+	add 6 7 7   		      		
         beq 3  4  case								        
-		beq 0 6 shift				
-        add 1 2 1
-		beq 0 0 multi				
-shift	add 4 4 4			        
+	beq 0 6 shift								
+shift	add 1 2 1
+	add 4 4 4			        
         add 2 2 2			        
         beq 0 0 multi		        
 case 	add 1 2 1				
-		beq 0 0 finish			
+	beq 0 0 finish			
 finish  halt
 zero .fill 0
 one .fill 1
